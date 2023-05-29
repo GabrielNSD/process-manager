33a44e063ca41d0f0843d22ab1f9019fb3feba65
gabrielnsd <gabrielnsdantas@gmail.com>
2023-05-29 19:04:40 -0300

    create processes list

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use libc::{
+    c_int, cpu_set_t, pid_t, sched_setaffinity, setpriority, CPU_SET, CPU_SETSIZE, PRIO_PROCESS,
+};
 use serde::Serialize;
 use std::fs;
 use std::process::{Command, Stdio};
@@ -50,7 +53,7 @@ fn get_process_usage(process_id: i32) -> Option<ProcessUsage> {
 
     let cpu_usage = fs::read_to_string(cpu_usage_path)
         .ok()?
-        .split(' ')
+        .split_whitespace()
         .nth(13)?
         .parse::<u64>()
         .ok()?;
@@ -156,13 +159,50 @@ fn send_process_signal(signal: &str, pid: &str) -> String {
     format!("kill result, {}", stdout_string)
 }
 
+#[tauri::command]
+fn set_process_priority(pid: i32, priority: i32) {
+    if set_priority(pid, priority) {
+        println!("Priority set successfully");
+    } else {
+        println!("Failed to set priority");
+    }
+
+    fn set_priority(pid: pid_t, priority: c_int) -> bool {
+        let result = unsafe { setpriority(PRIO_PROCESS, pid as u32, priority) };
+        result == 0
+    }
+}
+
+#[tauri::command]
+fn bind_process(pid: i32, cpu: u32) {
+    if bind_process_to_cpu(pid, cpu as i32) {
+        println!("Process bound successfully");
+    } else {
+        println!("Failed to bind process");
+    }
+
+    fn bind_process_to_cpu(pid: pid_t, cpu_id: c_int) -> bool {
+        let mut cpu_set: cpu_set_t = unsafe { std::mem::zeroed() };
+
+        // CPU affinity mask
+        unsafe {
+            CPU_SET(cpu_id as usize, &mut cpu_set);
+        };
+
+        let result = unsafe { sched_setaffinity(pid as i32, CPU_SETSIZE as usize, &cpu_set) };
+        result == 0
+    }
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             greet,
             list_processes,
             read_running_processes,
-            send_process_signal
+            send_process_signal,
+            set_process_priority,
+            bind_process
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ let pidInputEl: HTMLInputElement | null;
 let killBtnEl: HTMLElement | null;
 let stopBtnEl: HTMLElement | null;
 let contBtnEl: HTMLElement | null;
+let priorityInputEl: HTMLInputElement | null;
+let priorityBtnEl: HTMLElement | null;
+let cpuInputEl: HTMLInputElement | null;
+let cpuBtnEl: HTMLElement | null;
+let processListEl: HTMLElement | null;
+let filterInputEl: HTMLInputElement | null;
 
 let testMsgEl: HTMLElement | null;
 
@@ -28,11 +34,9 @@ async function test() {
 }
 
 async function getRunningProcesses() {
-  const list = await invoke("read_running_processes", {});
-
-  console.log(list);
-
-  setTimeout(getRunningProcesses, 1000);
+  // const list = await invoke("read_running_processes", {});
+  // console.log(list);
+  // setTimeout(getRunningProcesses, 1000);
 }
 
 async function sendSignal(signal: string) {
@@ -46,6 +50,82 @@ async function sendSignal(signal: string) {
   }
 }
 
+async function setPriority() {
+  if (pidInputEl && priorityInputEl && priorityBtnEl) {
+    console.log("setting priority", pidInputEl.value);
+    await invoke("set_process_priority", {
+      pid: parseInt(pidInputEl.value),
+      priority: parseInt(priorityInputEl.value),
+    });
+  }
+}
+
+async function setCPU() {
+  if (cpuInputEl && cpuBtnEl && pidInputEl) {
+    console.log("setting cpu", pidInputEl.value);
+    await invoke("bind_process", {
+      pid: parseInt(pidInputEl.value),
+      cpu: parseInt(cpuInputEl.value),
+    });
+  }
+}
+
+type Process = {
+  process_id: string;
+  process_name: string;
+  cpu_usage: number;
+  memory_usage: number;
+  user: string;
+  threads_used: number;
+};
+
+async function renderList() {
+  if (processListEl && filterInputEl) {
+    const list: Process[] = await invoke("read_running_processes", {});
+    processListEl.innerHTML = "";
+
+    list
+      .filter((el) =>
+        filterInputEl?.value
+          ? JSON.stringify(el)
+              .toLowerCase()
+              .includes(filterInputEl.value.toLowerCase())
+          : true
+      )
+      .forEach((process) => {
+        const row = document.createElement("tr");
+        const pid = document.createElement("td");
+        const name = document.createElement("td");
+        const cpu = document.createElement("td");
+        const memory = document.createElement("td");
+        const user = document.createElement("td");
+        const threads = document.createElement("td");
+        const executionTime = document.createElement("td");
+
+        pid.textContent = process.process_id;
+        name.textContent = process.process_name;
+        executionTime.textContent = "0";
+        cpu.textContent = process.cpu_usage.toFixed(4);
+        memory.textContent = process.memory_usage.toString();
+        user.textContent = process.user;
+        threads.textContent = process.threads_used.toString();
+
+        row.appendChild(name);
+        row.appendChild(cpu);
+        row.appendChild(executionTime);
+        row.appendChild(threads);
+        row.appendChild(memory);
+
+        row.appendChild(pid);
+        row.appendChild(user);
+
+        processListEl?.appendChild(row);
+      });
+
+    setTimeout(renderList, 1000);
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
@@ -54,7 +134,10 @@ window.addEventListener("DOMContentLoaded", () => {
     greet();
   });
 
-  getRunningProcesses();
+  filterInputEl = document.querySelector("#filter-input");
+  processListEl = document.querySelector("#process-list");
+
+  renderList();
 
   testMsgEl = document.querySelector("#test-msg");
   document.querySelector("#test-btn")?.addEventListener("click", (e) => {
@@ -67,6 +150,11 @@ window.addEventListener("DOMContentLoaded", () => {
   killBtnEl = document.querySelector("#kill-btn");
   stopBtnEl = document.querySelector("#stop-btn");
   contBtnEl = document.querySelector("#cont-btn");
+  priorityInputEl = document.querySelector("#priority-input");
+  priorityBtnEl = document.querySelector("#priority-btn");
+  cpuInputEl = document.querySelector("#cpu-input");
+  cpuBtnEl = document.querySelector("#cpu-btn");
+
   document.querySelector("#kill-btn")?.addEventListener("click", (e) => {
     e.preventDefault();
     sendSignal("KILL");
@@ -80,5 +168,15 @@ window.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#cont-btn")?.addEventListener("click", (e) => {
     e.preventDefault();
     sendSignal("CONT");
+  });
+
+  document.querySelector("#priority-btn")?.addEventListener("click", (e) => {
+    e.preventDefault();
+    setPriority();
+  });
+
+  document.querySelector("#cpu-btn")?.addEventListener("click", (e) => {
+    e.preventDefault();
+    setCPU();
   });
 });
